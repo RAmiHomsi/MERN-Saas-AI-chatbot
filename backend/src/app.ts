17d056cbb97b24eeddd72b5b379dacb14629ab15
RAmiHomsi <ramihomsi1997@hotmail.com>
2024-03-06 01:11:41 +0100

    deploy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,16 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "https://rami-saas-ai-chatbot.vercel.app/api",
+      "http://localhost:5173/api",
+    ],
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
