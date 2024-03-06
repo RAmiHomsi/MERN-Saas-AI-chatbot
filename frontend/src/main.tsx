46e71568b587e9936c5eb419e27ccba4833b70df
RAmiHomsi <ramihomsi1997@hotmail.com>
2024-03-06 01:04:10 +0100

    fixed cookies props

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
