800d35708f3562b1fd37fb482e32790e8083b144
RAmiHomsi <ramihomsi1997@hotmail.com>
2024-03-06 19:47:40 +0100

    deploy11

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,40 @@
-import app from "./app.js";
-import { connectToDatabase } from "./db/connection.js";
-
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { config } from "dotenv";
+import appRouter from "./routes/index.js";
+import mongoose from "mongoose";
+config();
+const app = express();
 const PORT = process.env.PORT || 5000;
-connectToDatabase()
+
+mongoose
+  .connect(process.env.MONGODB_URL)
   .then(() => {
-    app.listen(PORT, () => console.log("Server Open & Connected To Database"));
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log("Server is listening on port 5000");
+    });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+
+//middlewares
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://rami-saas-ai-chatbot.vercel.app/api",
+    ],
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.use("/api", appRouter);
+
+export default app;
